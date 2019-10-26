469edb54c3cf60a77173b68e2c71fd46547f70d1
Arkadiusz Rymarz <ak.rymarz@onet.com>
2019-10-26 11:19:52 +0200

    Dodaliśmy plik new_things w katalogu

---
+++ new_things/string_format.py
@@ -0,0 +1,14 @@
+# x = 5
+# krotka = ("element", "drugi_element")
+#
+# ciag_znakow = "Nasza liczba to {} blba baa {}, {}"
+#
+# print(f"Nasza liczba to {x + 10}") blba baa {krotka}")
+# print("Nasza liczba to {} blba baa {},{}".format(x+15, krotka, {2: "sd"}))
+# print(Nasza liczba to', x)
+
+Name = input("wpisz imie")
+Nazwisko = input("wpisz nazwisko")
+
+print("Twoje Imie",Name,"Twoje Nazwisko",Nazwisko)
+print(f"Twoje Imie {Name.capitalize()} Twpje Nazwisko {Nazwisko.capitalize()}")
